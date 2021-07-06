c47a31cc470f0a11eeec6028725bf59830f77228
Arman Doğru <arman.dogru@gmail.com>
2021-07-06 10:23:52 +0300

    Set up for project

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int WOEID = 23424969; //Where on Earth ID of the designated location
+        String topicName = "getir"; //The name of the trending topic
+        
+    }
+}
