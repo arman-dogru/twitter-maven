675750c453fc9339734c3c87ec66ed53f1e2bdef
Arman Doğru <arman.dogru@gmail.com>
2021-07-06 10:51:10 +0300

    Added Search trend method

---
+++ src/main/java/ConfigureTwitter.java
@@ -0,0 +1,38 @@
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class ConfigureTwitter {
+    ConfigurationBuilder cb;
+    Twitter twitter;
+    TwitterFactory tf;
+
+    public void config(String ConsumerKey, String AccessToken){
+        cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true);
+                cb.setOAuthConsumerKey(ConsumerKey);
+                //.setOAuthConsumerSecret("******************************************");
+                cb.setOAuthAccessToken(AccessToken);
+                //.setOAuthAccessTokenSecret("******************************************");
+        tf = new TwitterFactory(cb.build());
+        twitter = tf.getInstance();
+    }
+
+    //getir --> [getir, Getir, #getir]
+    public boolean searchTrend(String trendName, int WOEID) throws TwitterException {
+        String[] trendNameS = new String[3];
+        trendNameS[0] = trendName.substring(0,1).toLowerCase() + trendName.substring(1,-1);
+        trendNameS[1] = trendName.substring(0,1).toUpperCase() + trendName.substring(1,-1);
+        trendNameS[2] = "#"+trendName;
+
+        Trends trends = twitter.getPlaceTrends(WOEID);
+        Trend[] trendS = trends.getTrends();
+        for (Trend trend : trendS) {
+            for (String tName : trendNameS) {
+                if (trend.toString() == tName) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
