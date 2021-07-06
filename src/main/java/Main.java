92bd3c33d649715fa9dc8a7c0ccc93e12b76d94a
Arman Doğru <arman.dogru@gmail.com>
2021-07-06 10:28:10 +0300

    Created Configuration file for API

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int WOEID = 23424969; //Where on Earth ID of the designated location
         String topicName = "getir"; //The name of the trending topic
-        
+
     }
 }
