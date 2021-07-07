169cad4993bb536c1678bd306e01011749a43fff
Arman Doğru <arman.dogru@gmail.com>
2021-07-07 10:35:10 +0300

    hotfix on TrendData

--- src/main/java/TrendData.java
+++ src/main/java/TrendData.java
@@ -4,19 +4,16 @@ public class TrendData {
     public int tweetVolume;
     public String trendName;
     public boolean trending;
-    public Trend trend;
 
     public TrendData(String trendName){
-        this.trend = null;
         this.trendName = trendName;
         this.trending = false;
-        this.tweetVolume = 0;
+        this.tweetVolume = 0; //not Trending
     }
 
     public TrendData(Trend trend){
         tweetVolume = trend.getTweetVolume();
         trendName = trend.getName();
         this.trending = true;
-        this.trend = trend;
     }
 }
